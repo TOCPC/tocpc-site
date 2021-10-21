c47bc874a9b19ea247709b7abdb1b583b9aa7df2
Pongsaphol Pongsawakul <30414878+pongsaphol@users.noreply.github.com>
2021-10-21 05:36:47 +0000

    Add index and intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,140 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { MainFootage } from 'components/footage'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>TOCPC 2021</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">THACO 2021</h1>
+      <main className="relative w-full">
+        <div className="absolute right-full bg-red-400 w-20 h-full transform translate-x-2 sm:translate-x-10 md:translate-x-20 z-10"></div>
+        <div className="w-full">
+          <section className="w-full h-screen bg-gray-800 relative overflow-hidden">
+            <div className="block absolute h-full w-full">
+              <div className="relative h-full max-w-7xl">
+                <MainFootage />
+              </div>
+            </div>
+            <div className="flex flex-col w-full h-screen items-center justify-center">
+              <div>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  Thailand
+                </p>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  Online
+                </p>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  Competitive
+                </p>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  Programming
+                </p>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  Contest
+                </p>
+                <p className="py-1 font-extrabold text-5xl text-white text-center">
+                  2021
+                </p>
+                <p className="py-6 text-white text-center">20-21 NOV 2021</p>
+              </div>
+              <div className="pt-6">
+                <button className="bg-red-400 px-10 py-6 text-white text-2xl font-semibold rounded-full">
+                  Register
+                </button>
+                <p className="py-4 text-white text-center">until 19 NOV 2021</p>
+              </div>
+            </div>
+          </section>
+          <section className="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-24 w-full h-auto bg-gray-900 px-24 md:px-48 py-36">
+            <div className="flex flex-col">
+              <div className="bg-red-400 rounded-lg p-2 w-min">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                  />
+                </svg>
+              </div>
+              <p className="text-white font-bold text-2xl py-6">ห้ามพลาด</p>
+              <p className="text-white py-2">
+                การแข่งขันจะจัดขึ้นในวันเสาร์ที่ 20 และวันอาทิตย์ที่ 21
+                ของเดือนพฤศจิกายน พ.ศ.2564 สามารถดูกำหนดการเพิ่มเติมได้ที่นี่
+              </p>
+              <p className="text-white font-bold text-xl py-4 underline">
+                กำหนดการ
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <div className="bg-red-400 rounded-lg p-2 w-min">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                  />
+                </svg>
+              </div>
+              <p className="text-white font-bold text-2xl py-6">
+                ต้องการแต่งโจทย์?
+              </p>
+              <p className="text-white py-2">
+                เราพร้อมให้ทุกคนมีส่วนร่วมในการแต่งโจทย์ หากต้องการร่วมงานกับเรา
+                สามารถศึกษากระบวนการแต่งโจทย์และการส่งโจทย์ได้ตามนี้
+              </p>
+              <p className="text-white font-bold text-xl py-4 underline">
+                กระบวนการแต่งโจทย์
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <div className="bg-red-400 rounded-lg p-2 w-min">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                  />
+                </svg>
+              </div>
+              <p className="text-white font-bold text-2xl py-6">
+                รูปแบบการแข่งขัน
+              </p>
+              <p className="text-white py-2">
+                การแข่งขันยาวรอบละ 3 ชั่วโมง จัดขึ้น 2 รอบ
+                โดยมีโจทย์การแข่งขันรอบละ 3 ข้อ
+                สามารถดูรายละเอียดโจทย์แต่ละข้อและขอบเขตเนื้อหาที่ใช้ในการแข่งขันได้ที่นี่
+              </p>
+              <p className="text-white font-bold text-xl py-4 underline">
+                รูปแบบการแข่งขัน
+              </p>
+            </div>
+          </section>
+        </div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
