9aa2df38d2a09df6b4238c35a6fd58301ec75410
Pongsaphol Pongsawakul <30414878+pongsaphol@users.noreply.github.com>
2021-10-27 23:23:59 +0000

    Fix svg properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,9 +97,9 @@ const Home: NextPage = () => {
                         fill="currentColor"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         />
                       </svg>
                     </span>
@@ -159,9 +159,9 @@ const Home: NextPage = () => {
                         fill="currentColor"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         />
                       </svg>
                     </span>
