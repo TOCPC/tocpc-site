556a7047cd14abefc7ef269aac30d213cf307765
Pongsaphol Pongsawakul <30414878+pongsaphol@users.noreply.github.com>
2021-10-28 07:58:11 +0000

    Add auth

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext, useCallback } from 'react'
 import Router from 'next/router'
-import cookie from 'js-cookie'
 
 import {
   getAuth,
@@ -10,16 +9,22 @@ import {
   GithubAuthProvider,
   signInWithPopup,
   signOut,
-  onIdTokenChanged,
 } from 'firebase/auth'
+import { onSnapshot } from 'firebase/firestore'
 import firebaseApp from './firebase'
-import { createUser, getCurrentUserData } from './db'
-import { AUTH_COOKIE } from './constants'
+import { createUser, getUserRef } from './db'
+import { Loading } from 'components/Loading'
 
-export interface IUserData extends IInitialUserData {
-  admin: boolean
-  passedTask: Object
+interface UserData extends IInitialUserData {
   username: string
+  password: string
+  firstname?: string
+  lastname?: string
+  tel?: string
+  address?: string
+  postalcode?: string
+  size?: string
+  anonymous: boolean
 }
 
 interface IInitialUserData {
@@ -33,14 +38,12 @@ interface IInitialUserData {
 const auth = getAuth(firebaseApp)
 
 interface IAuthContext {
-  user: User | null
-  userData: IUserData | null
+  user: UserData | null
   loading: boolean
   signinWithFacebook: (redirect: string) => Promise<void>
   signinWithGoogle: (redirect: string) => Promise<void>
   signinWithGitHub: (redirect: string) => Promise<void>
   signout: () => void
-  updateUserData: () => Promise<void>
 }
 
 const AuthContext = React.createContext<IAuthContext | null>(null)
@@ -52,6 +55,7 @@ export const useAuth = () => {
 export const AuthProvider: React.FC = ({ children }) => {
   const auth = useProvideAuth()
 
+  return <Loading />
   // if (auth.loading) {
   //   return <Loading />
   // }
@@ -60,41 +64,36 @@ export const AuthProvider: React.FC = ({ children }) => {
 }
 
 function useProvideAuth() {
-  const [user, setUser] = useState<User | null>(null)
-  const [userData, setUserData] = useState<IUserData | null>(null)
+  const [user, setUser] = useState<UserData | null>(null)
   const [loading, setLoading] = useState(true)
 
-  const updateUserData = useCallback(async () => {
-    const data = await getCurrentUserData(user?.uid)
-
-    if (data) {
-      setUserData(data as IUserData)
-    } else {
-      setUserData(null)
+  useEffect(() => {
+    if (user?.uid) {
+      const userRef = getUserRef(user.uid)
+      const unsubscribe = onSnapshot(userRef, (doc) => {
+        setUser({
+          ...user,
+          ...doc.data(),
+        })
+        setLoading(false)
+      })
+      return () => unsubscribe()
     }
   }, [user?.uid])
-
-  useEffect(() => {
-    if (user) {
-      updateUserData()
-    }
-  }, [user, updateUserData])
 
   const handleUser = async (rawUser: User | null) => {
     if (rawUser) {
       const user = formatUser(rawUser)
-      createUser(user.uid, user)
-      setUser(rawUser)
-
-      cookie.set(AUTH_COOKIE, 'true', {
-        expires: 1,
-      })
-
-      setLoading(false)
+      await createUser(user.uid, user)
+      const tempUser: UserData = {
+        ...user,
+        username: '',
+        password: '',
+        anonymous: true,
+      }
+      setUser(tempUser)
     } else {
       setUser(null)
-      cookie.remove(AUTH_COOKIE)
-
       setLoading(false)
     }
   }
@@ -142,21 +141,13 @@ function useProvideAuth() {
     return await handleUser(null)
   }
 
-  useEffect(() => {
-    const unsubscribe = onIdTokenChanged(auth, handleUser)
-
-    return () => unsubscribe()
-  }, [])
-
   return {
     user,
-    userData,
     loading,
     signinWithFacebook,
     signinWithGoogle,
     signinWithGitHub,
     signout,
-    updateUserData,
   }
 }
 
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -10,32 +10,18 @@ import firebaseApp from './firebase'
 
 const db = getFirestore(firebaseApp)
 
+export const getUserRef = (uid: string) => {
+  return doc(db, 'users', uid)
+}
+
 export const updateUser = (uid: string, data: DocumentData): Promise<void> => {
-  const userRef = doc(db, 'users', uid)
+  const userRef = getUserRef(uid)
 
   return updateDoc(userRef, data)
 }
 
 export const createUser = (uid: string, data: DocumentData): Promise<void> => {
-  const userRef = doc(db, 'users', uid)
+  const userRef = getUserRef(uid)
 
   return setDoc(userRef, data, { merge: true })
-}
-
-export const getCurrentUserData = async (
-  uid: string | undefined
-): Promise<null | DocumentData> => {
-  if (uid) {
-    const userRef = doc(db, 'users', uid)
-
-    const docSnap = await getDoc(userRef)
-
-    if (docSnap.exists()) {
-      return docSnap.data()
-    } else {
-      return null
-    }
-  }
-
-  return null
 }
